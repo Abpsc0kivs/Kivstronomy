e45b58ef734161c58aabd09b9c7cdc5d3f6020db
Abhinav Pratap Singh Chouhan <singh.abhinav0704@gmail.com>
2023-06-23 12:59:14 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -11,4 +11,9 @@ if(hournow >18){
 }else{
     greeting='Welcome!';
 }
-document.write('<h2>'+greeting+'</h2>');
+var username = prompt("Enter Your Name: ");
+alert(username + " is visiting Kivstronomy By Ab.PSC at " + today );
+var letter1 = username.slice(0,1);
+var letterRest = username.slice(1,username.length);
+var Name = letter1.toUpperCase()+letterRest.toLowerCase();
+document.write('<h2>'+greeting + " " +Name+ '</h2>');
